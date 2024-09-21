c02e6e48e6960ab1074c552521e2addab52e1160
sam <bafnasamidha@gmail.com>
2024-09-21 12:07:03 +0530

    commit for get request

--- index.js
+++ index.js
@@ -6,18 +6,7 @@ const users=require("./MOCK_DATA (1).json");
 
 const app= express();
 
-app.get("/api/user",(req,res)=>{
-
-    res.send(users);
-    //it directly convert the content-type as the type of content 
-
-    // res.json(users);
-    //it is directly convert js object to JSON format
-
-    // res.end(JSON.stringify(users));
-    //we can convert it to JSON format through JSON.stringfy() function 
-});
-
+//--server side rendering
 app.get("/user",(req,res)=>{
     const html=`
     <ul>
@@ -33,6 +22,25 @@ app.get("/user",(req,res)=>{
     `;
     res.send(html);
 });
+
+//--client side rendering
+app.get("/api/user",(req,res)=>{
+
+    res.send(users);
+    //it directly convert the content-type as the type of content 
+
+    // res.json(users);
+    //it is directly convert js object to JSON format
+
+    // res.end(JSON.stringify(users));
+    //we can convert it to JSON format through JSON.stringfy() function 
+});
+
+app.get("/api/user/:id",(req,res)=>{
+    const id=req.params.id;
+    const user=users.find((user)=>user.id==id);
+    res.json(user);
+})
 
 app.listen(8000,()=>{
     console.log("server started");
